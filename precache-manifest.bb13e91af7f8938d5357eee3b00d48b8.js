1950d99925dbe06c1145bfbccb11a444d5cc8cf0
Kunal Shinde <kunalshinde373@gmail.com>
2020-05-20 03:41:21 +0530

    Updates

---
+++ precache-manifest.bb13e91af7f8938d5357eee3b00d48b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5ba9867d8e973704f0415dbb38133c",
+    "url": "/weather-application/index.html"
+  },
+  {
+    "revision": "607e2140012ae1f0ac1e",
+    "url": "/weather-application/static/css/main.3aaa5e5d.chunk.css"
+  },
+  {
+    "revision": "1a1e18d21ebeeed26496",
+    "url": "/weather-application/static/js/2.3bfd6f96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-application/static/js/2.3bfd6f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607e2140012ae1f0ac1e",
+    "url": "/weather-application/static/js/main.36dc9f20.chunk.js"
+  },
+  {
+    "revision": "2be2fe7f8241c0aa3f5d",
+    "url": "/weather-application/static/js/runtime-main.b4928d7c.js"
+  }
+]);
